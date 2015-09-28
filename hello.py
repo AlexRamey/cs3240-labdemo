450d97d4dd5bdf9fbaadcaa6e9a2a511b5d1bc21
Alex Ramey <abr8xq@virginia.edu>
2015-09-28 14:25:35 -0400

    Moved greeting(msg) function to helper module

--- hello.py
+++ hello.py
@@ -1,9 +1,6 @@
 # CS3240 Lab 5 PreLab
 # Alex Ramey (abr8xq)
 
-
-def greeting(msg):
-    print(msg)
-
+from helper import greeting
 
 greeting("hello")
---
+++ helper.py
@@ -0,0 +1,5 @@
+# CS3240 Lab 5
+# Alex Ramey (abr8xq)
+
+def greeting(msg):
+    print(msg)
